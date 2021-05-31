818b653be11ffc2fce5d931668efa49f04359708
Emanuel Moecklin <emanuel.moecklin@gmail.com>
2021-05-31 14:12:24 -0400

    migrate to the latest AGP and Hilt plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,10 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-beta02")
+        classpath("com.android.tools.build:gradle:7.0.0-beta03")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.0")
         classpath("com.github.triplet.gradle:play-publisher:3.4.0-agp7.0")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.35")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.36")
         classpath("io.github.funnydevs:hilt-conductor-plugin:0.2.0")
     }
 }
